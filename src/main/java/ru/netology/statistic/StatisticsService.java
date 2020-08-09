f542927d436d1e4eca53c1e628b898b6ccf3bd28
Uliana Utina <ulyana190909@yandex.ru>
2020-08-09 23:56:15 +0300

    проверка тестов

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -7,12 +7,23 @@ public class StatisticsService {
    * @param incomes - array of incomes
    * @return - index of first max value
    */
+
   public long findMax(long[] incomes) {
+    long current_max_index = 0;
+    long current_max = incomes[0];
+    for (long income: incomes)
+      if (current_max < income)
+        current_max = income;
+
+    return current_max;
+  }
+
+  public long findMaximum(long[] incomes) {
     long current_max_index = 0;
     long current_max = incomes[0];
     for (long income : incomes)
       if (current_max < income)
         current_max = income;
-        return current_max;
+    return current_max;
   }
 }
--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StatisticsServiceTest {
 
+
   @Test
   void findMax() {
     StatisticsService service = new StatisticsService();
@@ -14,6 +15,18 @@ class StatisticsServiceTest {
     long expected = 12;
 
     long actual = service.findMax(incomesInBillions);
+
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void findMaxTwo() {
+    StatisticsService service = new StatisticsService();
+
+    long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 45, 5, 11, 11, 12};
+    long expected = 3;
+
+    long actual = service.findMaximum(incomesInBillions);
 
     assertEquals(expected, actual);
   }
