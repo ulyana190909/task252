a6af80b1920b52fe1d0294b43ba4b001bdd2e027
Uliana Utina <ulyana190909@yandex.ru>
2020-08-10 20:48:24 +0300

    корректировка тестов, для 100% покрытия

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -8,22 +8,21 @@ public class StatisticsService {
    * @return - index of first max value
    */
 
-  public long findMax(long[] incomes) {
-    long current_max_index = 0;
-    long current_max = incomes[0];
-    for (long income: incomes)
-      if (current_max < income)
-        current_max = income;
+  public int findMax(long[] incomes) {
+    int current_max_index = 0;
+    for (int i = 1; i < incomes.length; i++)
+      if (incomes[i] >= incomes[current_max_index])
+        current_max_index = i;
 
-    return current_max;
+    return current_max_index;
   }
 
-  public long findMaximum(long[] incomes) {
-    long current_max_index = 0;
-    long current_max = incomes[0];
+
+  public long findMinimum(long[] incomes) {
+    long current_min = incomes[0];
     for (long income : incomes)
-      if (current_max < income)
-        current_max = income;
-    return current_max;
+      if (current_min > income)
+        current_min = income;
+    return current_min;
   }
 }
